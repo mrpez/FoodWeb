70d549eff795f3d332c4654230a0669f5204a9f3
M. Ryan Lopez <mlopez6@ycp.edu>
2014-03-27 15:43:28 -0400

    Login Model
    Basics for the login model class

---
+++ webRoot/class/Model/Login.php
@@ -0,0 +1,34 @@
+<?php
+
+	if( !class_exists('Utility') ) {
+		include(dirname(__FILE__ . '/../Utility.php'));
+		$Utility = new Utility;
+	}
+	
+	class Login extends Utility {
+		
+		private $isLoggedIn = false;
+		private $userIndex = null;
+		
+		public function getLoginStatus() {
+			return $this->$isLoggedIn;
+		}
+		
+		public function getUserIndex() {
+			if( $this->isLoggedIn != true ) {
+				return false;
+			}
+			return $this->$userIndex;
+		}
+		
+		public function setLoginStatus($loginStatus = false) {
+			$this->$loginStatus = $loginStatus;
+		}
+		
+		public function setUserIndex($userIndex = false) {
+			$this->$userIndex = $userIndex;
+		}
+		
+	}
+
+?>
