6f6cd2364759eb7792fea59b62b5168cb49d7ec5
Ryan Lopez <mlopez6@ycp.edu>
2014-03-28 13:25:23 -0400

    Unit Tests

---
+++ webRoot/unit-tests/LoginUnitTests.php
@@ -0,0 +1,51 @@
+<?php
+
+	echo 'Starting Login Model Test...<br />';
+	
+	include(dirname(__FILE__) . '/../class/Model/Login.php');
+	$Login = new Login;
+	
+	echo 'Testing Logged In Attribute<br />';
+	
+	$Login->setLoginStatus(true);
+	if($Login->getLoginStatus() == true) {
+		echo 'Passed... <br />';
+	} else {
+		echo 'Failed... <br />';
+	}
+	
+	$Login->setLoginStatus(false);
+	if($Login->getLoginStatus() == false) {
+		echo 'Passed... <br /><br />';
+	} else {
+		echo 'Failed... <br /><br />';
+	}
+	
+	echo 'Testing User Index Attribute<br />';
+	
+	$errorCount = 0;
+	$testCount = 0;
+	
+	$testCount++;
+	if($Login->getUserIndex() !=== false) {
+		echo 'Passed... <br />';
+	} else {
+		$errorCount++;
+		echo 'Failed... <br />';
+	}
+	
+	$testCount++;
+	$Login->setUserIndex(12456);
+	
+	$Login->setLoginStatus(false);
+	if($Login->getLoginStatus() == false) {
+		echo 'Passed... <br /><br />';
+	} else {
+		$errorCount++;
+		echo 'Failed... <br /><br />';
+	}
+	echo (($errorCount/$testCount) * 100) . '% Error Rate (' . $errorCount . '/' . $testCount . ')';
+
+
+
+?>
