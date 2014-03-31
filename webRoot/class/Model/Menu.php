4580bbc9503a24c1a43e9235313ce6bbdfed93c8
mdichman <mdichman@ycp.edu>
2014-03-31 13:47:21 -0400

    Menu and Item Model Class updates
    updated menu and item classes. Not too sure what to do about item price
    and sides

--- webRoot/class/Model/Menu.php
+++ webRoot/class/Model/Menu.php
@@ -4,8 +4,28 @@
 		include(dirname(__FILE__) . '/../Utility.php');
 		$Utility = new Utility;
 	}	
-	
+	echo 'this is the menu!';
 	class Menu extends Utility {
+		private $menu;
+		private $menuItems = array(1);
+		private $menuSides = array(1);
+		
+		//get items from database and populate menuItems array
+		public function getMenuItems($x){
+			$this->menuItems[] = $x;
+		}
+		//get sides from database and populate menuSides array
+		public function getMenuSideItems($x){
+			$this->menuSides[] = $x;
+		}
+		//allow vendor to add items to the data
+		public function setMenuItems(){
+			 return $this->menuItems;
+		}
+		//allow vendor to add sides to the data
+		public function setMenuSides(){
+			 return $this->menuSides;
+		}
 		
 
 		
