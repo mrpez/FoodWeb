2634d68a9259bcf1be83966267b2b7e3b08ec4fb
M Ryan Lopez <mlopez6@ycp.edu>
2014-04-02 15:29:37 -0400

    Added Vendor Home Link

--- webRoot/index.php
+++ webRoot/index.php
@@ -5,8 +5,11 @@ include("header.php");
 echo 'This is the FoodWeb Homepage. Login to start!<br /><br />
 	  <h3>Actions</h3>
 	  <ul>
+		<li><a href="/vendor-home.fw">Vendor Home</a></li>
 		<li><a href="/vendor-signup.fw">Become a Vendor</a></li>
 	  </ul>';
+	  
+	  var_dump($LoginController->getUserIndex());
 
 
 include("footer.php");
