71cb976fb1d196c7023d5aa3eb998e99a79569a5
M Ryan Lopez <mlopez6@ycp.edu>
2014-04-02 17:10:59 -0400

    Added Worksheet

---
+++ webRoot/class/View/worksheet.php
@@ -0,0 +1,52 @@
+<h1>FoodWeb Worksheet</h1>
+<ul>
+	<li>
+		<h2>Vendors</h2>
+		<ol>
+			<li>
+				Vendor Settings
+				<ul>
+					<li>Hours</li>
+					<li>Location</li>
+					<li>Categories/Tags</li>
+					<li>Menu</li>
+				</ul>			
+			</li>
+			<li>
+				Menu
+				<ul>
+					<li>Category Tree</li>
+					<li>Editor</li>
+					<li>Viewer</li>
+				</ul>			
+			</li>
+			<li>
+				Products
+				<ul>
+					<li>Photos</li>
+					<li>Editor for Descriptions</li>
+					<li>
+						Sides
+						<ul>
+							<li>Creating Side Groups</li>
+							<li>Assigning a Side Group to a Menu Item</li>
+						</ul>
+					</li>
+				</ul>			
+			</li>
+		</ol>
+	</li>
+	<li>
+		<h2>Users</h2>
+		<ol>
+			<li>
+				User Settings
+				<ul>
+					<li>Home Location - Use this for coupons & offers</li>
+					<li>Upload Image</li>
+					<li>Saved Payment - (Pandoras Box)</li>
+				</ul>
+			</li>
+		</ol>
+	</li>
+</ul>
