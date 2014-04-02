2d316381db938c16299a36abcc0dcc1845a04a26
M Ryan Lopez <mlopez6@ycp.edu>
2014-04-02 15:29:29 -0400

    Added Menu & Vendor Tables

--- webRoot/admin-functions/populate-database.php
+++ webRoot/admin-functions/populate-database.php
@@ -18,8 +18,8 @@
 	$query = $dbCon->prepare("CREATE TABLE users
 							  (
 								id integer auto_increment not null primary key
-								, name varchar(100)
-								, email integer unique
+								, name varchar(500)
+								, email varchar(500) unique
 							  )");
 	if(!$query->execute())
 		var_dump($query->errorInfo());
@@ -32,6 +32,69 @@
 								, user_id integer
 								, password varchar(255)
 								, Foreign Key (user_id) references users(id)
+							  )");
+	if(!$query->execute())
+		var_dump($query->errorInfo());
+		
+	
+	$query = $dbCon->prepare("CREATE TABLE vendors
+							  (
+								id integer auto_increment not null primary key
+								, owner_id integer
+								, name varchar(500)
+								, Foreign Key (owner_id) references users(id)
+							  )");
+	if(!$query->execute())
+		var_dump($query->errorInfo());
+	
+	$query = $dbCon->prepare("CREATE TABLE menus
+							  (
+								id integer auto_increment not null primary key
+								, vendor_id integer
+								, name varchar(500)
+								, Foreign Key (vendor_id) references vendors(id)
+							  )");
+	if(!$query->execute())
+		var_dump($query->errorInfo());
+	
+	$query = $dbCon->prepare("CREATE TABLE menu_photos
+							  (
+								id integer auto_increment not null primary key
+								, menu_id integer
+								, caption varchar(500)
+								, photo_name varchar(500)
+								, Foreign Key (menu_id) references menus(id)
+							  )");
+	if(!$query->execute())
+		var_dump($query->errorInfo());
+	
+	$query = $dbCon->prepare("CREATE TABLE menu_categories
+							  (
+								id integer auto_increment not null primary key
+								, name varchar(500)
+							  )");
+	if(!$query->execute())
+		var_dump($query->errorInfo());
+	
+	$query = $dbCon->prepare("CREATE TABLE menu_items
+							  (
+								id integer auto_increment not null primary key
+								, name varchar(500)
+								, price decimal(8,2)
+								, description varchar(5000)
+							  )");
+	if(!$query->execute())
+		var_dump($query->errorInfo());
+			
+	$query = $dbCon->prepare("CREATE TABLE menu_category_tree
+							  (
+								id integer auto_increment not null primary key
+								, menu_id integer
+								, item_id integer
+								, item_type integer
+								, left_pointer integer
+								, right_pointer integer
+								, Foreign Key (menu_id) references menus(id)
 							  )");
 	if(!$query->execute())
 		var_dump($query->errorInfo());
