4fcba6243a84c759f706ea2e3c8164a01a51c544
BrandonCope <bcope@ycp.edu>
2014-04-07 13:37:37 -0400

    Still Working on resets

--- webRoot/class/PreView/reset.php
+++ webRoot/class/PreView/reset.php
@@ -3,9 +3,17 @@
 		include(dirname(__FILE__) . '/../Controller/UserController.php');
 		$UserController = new UserController;
 	}
+					
 	$errorString = '';
 	$showForm = true;
 
+
 	
 	if( !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) )
-		$errorString .= 'Please provide a valid email address to reset password.<br />';
+		$errorString .= 'Please provide a valid email address to reset password.<br/>';	
+	
+	if( strlen($errorString) == 0 ) {
+		$showForm = !$UserController->forgotPass($_POST['email']);
+	}
+
+?>
