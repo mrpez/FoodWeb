1f8ca904826fdd638b8ab3ed012c5304e4637fa6
M Ryan Lopez <mlopez6@ycp.edu>
2014-04-10 22:43:37 -0400

    Updated View Controller so Get Variables Work

--- webRoot/view.php
+++ webRoot/view.php
@@ -1,5 +1,17 @@
 <?php
 	
+	$vars = explode('?', $_SERVER['REQUEST_URI']);
+	
+	if( count($vars) > 1 ) {
+		$vars = explode('&', $vars[1]);
+		for($i = 0; $i < count($vars); $i++) {
+			$thisVar = explode('=', $vars[$i]);
+			if( count($thisVar) > 1 ) {
+				$_GET[$thisVar[0]] = $thisVar[1];
+			}
+		}
+	}
+	
 	if( !array_key_exists('view', $_GET) ) {
 		$_GET['view'] = '';
 	}
