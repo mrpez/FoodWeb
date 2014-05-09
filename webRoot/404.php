f28244dcffcb7dee73a3877880d8c7588a0fc50e
M Ryan Lopez <mlopez6@ycp.edu>
2014-05-09 19:55:11 -0400

    404 Page

---
+++ webRoot/404.php
@@ -0,0 +1,12 @@
+<?php
+	
+include('header.php');
+
+echo '<h1>Not Found!</h1>
+	  <p>
+		The page you were looking for was not found! You can try to find it from our <a href="/">home page</a>.
+	  </p>';
+	
+include('footer.php');	
+	
+?>
