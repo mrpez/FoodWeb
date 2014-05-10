e8054dcc8d4485401a9d56ef977b651fff370459
Jordan <jspecht@ycp.edu>
2014-05-09 20:28:42 -0400

    More Product page
    Added More Food links

--- webRoot/class/View/product.php
+++ webRoot/class/View/product.php
@@ -1,28 +1,45 @@
 <style type="text/css">
-	div#menuLeftPane {
-		float: left;
-		width: 35%;
+	div#productTitle{
+		padding-left:10px;
 	}
-	div#menuRightPane {
+	div#productMoreTitle{
+		padding-top:30px;
+		float: left;
+		font-size:150%;
+	}
+	div#productImage {
+		float: left;
+		min-width:170px;
+		width: 35%;
+		padding-left:10px;
+	}
+	div#productInfo {
 		float:left;
 		width: 60%;
 	}
-	.floatstop {
+	div#productMore {
+		float:left;
+		padding-left:10px;
+		padding-right:10px;
+		width: 5%;
+		min-width:80px;
+	}
+	div#clearStop {
 		clear:both;
 	}
 </style>
 
-<div>
+<div id="productTitle">
 	<h1><?php echo $ProductController->getName(); ?> </h1>
 </div>
 
-<div id="menuLeftPane">
+<div id="productImage">
 	<p>
 		<img src="class/View/cheese.jpg" alt="Good Image" width="150" height="150">
 	</p>
 </div>
 
-<div id="menuRightPane">
+<div id="productInfo">
 	<p>
 		<b>Price: $</b> <?php echo $ProductController->getPrice();?><br><br>
 		<b>Description:</b><br>
@@ -33,4 +50,30 @@
 		</form>
 	</p>
 </div>
-<div id=".floatstop"></div>
+
+<div id="clearStop"></div>
+
+<div id="productMoreTitle">
+	<b>More From This Vendor</b><br>
+</div>
+
+<div id="clearStop"></div>
+
+<div id="productMore">
+	<img src="class/View/cheese.jpg" alt="More Food" width="60" height="60">
+	<button type="button">Check It Out!</button>
+</div>
+
+<div id="productMore">
+	<img src="class/View/cheese.jpg" alt="More Food" width="60" height="60">
+	<button type="button">Check It Out!</button>
+</div>
+
+<div id="productMore">
+	<img src="class/View/cheese.jpg" alt="More Food" width="60" height="60">
+	<button type="button">Check It Out!</button>
+</div>
+
+
+
+
